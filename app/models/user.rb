d5834845e7a416152be204e6cf056edd32519503
Willemina SB <wstikk@gmail.com>
2016-05-13 14:23:49 -0700

    User functionality added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,17 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :name, presence: true
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
 
   validates :email, presence: true, uniqueness: true
 
   validates :password,
     length: { in: 5..20 }, on: :create
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 
 end
