f12a05cb0263d369c973aaa9269c860d1421ccd8
Mike Conlin <mikeyconlin@gmail.com>
2016-05-15 13:20:05 -0700

    started handlebars templating

--- app/assets/javascripts/hikes.js
+++ app/assets/javascripts/hikes.js
@@ -10,15 +10,23 @@ $(function(){
       success: function(hikes){
         $('#all_hikes').remove();
         $('#searched_hikes').find('tbody').empty();
-        hikes.forEach(function(hike){
-          $('<tr>').addClass('new_row')
-          .append($('<td>').text(hike.name).attr("href", "/hikes"+hike.id))
-          .append($('<td>').text(hike.distance_in_km + ' km'))
-          .append($('<td>').text(hike.time_in_hours + ' hours'))
-          .append($('<td>').text(hike.difficulty))
-          .appendTo('#searched_hikes');
+        var Source = document.getElementById("Handlebars-Template").textContent;
+        var Template = Handlebars.compile(Source);
+        var HTML = Template({ Hikes : hikes });
+
+        // hikes.forEach(function(hike){
+        //   var html = template(hike);
+          // $('<tr>').addClass('new_row')
+          // .append($('<td>').text(hike.name).attr("href", "/hikes"+hike.id))
+          // .append($('<td>').text(hike.distance_in_km + ' km'))
+          // .append($('<td>').text(hike.time_in_hours + ' hours'))
+          // .append($('<td>').text(hike.difficulty))
+          // .appendTo('#searched_hikes');
         });
       }
     });
   });
 });
+
+
+
